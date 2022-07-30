390418cfdeff4cc23783c0b3ec25fd9fd6051a88
Mirwais Mir <Mirwais.mirzakwal1@gmail.com>
2022-07-30 18:06:27 -0400

    print mirwais class

--- src/main/java/paractice/purpose/HelloWorld.java
+++ src/main/java/paractice/purpose/HelloWorld.java
@@ -2,6 +2,7 @@ package paractice.purpose;
 
 public class HelloWorld {
 	public static void main(String[] args) {
+		// i brough some changes
 		System.out.println("Heloo world");
 	}
 
---
+++ src/main/java/tekschool/traninig/PrintingMyName.java
@@ -0,0 +1,9 @@
+package tekschool.traninig;
+
+public class PrintingMyName {
+	public static void main(String[] args) {
+		
+		System.out.println("My name is Mirwais Mir ");
+	}
+
+}
