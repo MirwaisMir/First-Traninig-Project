f81608d89e489818fa406a946e130ab8be0b0362
Mirwais Mir <Mirwais.mirzakwal1@gmail.com>
2022-07-30 18:49:11 -0400

    This changes is for New Testers Branch

--- src/main/java/tekschool/traninig/PrintingMyName.java
+++ src/main/java/tekschool/traninig/PrintingMyName.java
@@ -4,6 +4,7 @@ public class PrintingMyName {
 	public static void main(String[] args) {
 		// i want make some changes
 		// Second changes
+		// This changes for New testers branch
 		System.out.println("My name is Mirwais Mir ");
 	}
 
