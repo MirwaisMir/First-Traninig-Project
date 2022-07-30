8d6802c1af72ed702d02207590b5a4838485e236
Mirwais Mir <Mirwais.mirzakwal1@gmail.com>
2022-07-30 18:46:11 -0400

    i want to push this to New testers branch

--- src/main/java/tekschool/traninig/PrintingMyName.java
+++ src/main/java/tekschool/traninig/PrintingMyName.java
@@ -2,7 +2,8 @@ package tekschool.traninig;
 
 public class PrintingMyName {
 	public static void main(String[] args) {
-		
+		// i want make some changes
+		// Second changes
 		System.out.println("My name is Mirwais Mir ");
 	}
 
